b47cbf9cc827e8c594aa71a0d3ec443e22f1126f
Odonno <bottiau.david@laposte.net>
2022-10-29 00:40:21 +0200

    ⬆️ migrate to nextjs 13

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "../styles/tailwind.css";
+import "../styles/globals.css";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const RootLayout = ({ children }: Props) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
