d3ec2c7aa54566d7aa583e9576e38a1830283718
Odonno <bottiau.david@laposte.net>
2022-10-31 09:34:31 +0100

    ♻️ create head component

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { NextPage } from "next";
-import Head from "next/head";
 import { motion } from "framer-motion";
 import * as Atoms from "../state/index";
 import { useAtom } from "jotai";
@@ -20,15 +19,6 @@ const Home: NextPage = () => {
 
   return (
     <div className="text-white">
-      <Head>
-        <title>Iron Maiden Blindtest</title>
-        <meta
-          name="description"
-          content="Can you remember these Iron Maiden songs?"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-center min-h-screen max-h-screen overflow-hidden">
         <h1 className="flex flex-col text-center font-iron-maiden my-6">
           <span className="neon-red text-4xl text-red-600">Iron Maiden</span>
---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+const Head = () => {
+  return (
+    <>
+      <title>Iron Maiden Blindtest</title>
+      <meta
+        name="description"
+        content="Can you remember these Iron Maiden songs?"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+};
+
+export default Head;
